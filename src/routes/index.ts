f9c154dd1f305899ceb262bcc59e18de7f60561a
Thiago <thiagofang@gmail.com>
2022-06-10 11:00:54 -0300

    adicionando rotas no servidor, views e mais alguns detalhes pequenos

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,14 @@
-import { Router, Request, Response } from 'express';
+import { Router} from 'express';
+import * as searchController from '../controllers/searchController'
+import * as pageController from '../controllers/pageController'
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-    res.send('Home')
-});
+router.get('/', pageController.home);
+router.get('/dogs', pageController.dogs);
+router.get('/cats', pageController.cats);
+router.get('/fishes', pageController.fishes);
+
+router.get('/search', searchController.search);
 
 export default router;
