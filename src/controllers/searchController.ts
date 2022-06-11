91eb2bea093011bf2b91b23f6ae1d4cd0d932515
Thiago <thiagofang@gmail.com>
2022-06-11 15:34:49 -0300

    adicionando sistema de pesquisa e página de 404

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from "express";
 
+import { createMenuObject } from "../helpers/createMenuObject";
+import { Pet } from '../models/pet';
+
 export const search = (req: Request, res: Response) => {
-    //res.render('pages/search')
+    const query: string = req.query.q as string;
+    if(!query) {
+        res.redirect('/');
+        return
+    }
+    const list = Pet.getFromName(query);
+    console.log(list)
+
+    res.render('pages/page',{
+        menu: createMenuObject(''),
+        list,
+        query
+    });
 };
